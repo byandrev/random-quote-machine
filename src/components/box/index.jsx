753df26ac1148ff69d391f6d0c6e7f751ac5a931
byandrev <byandrev@gmail.com>
2023-06-07 16:15:58 -0500

    add button for copy quote and animations

--- src/components/box/index.jsx
+++ src/components/box/index.jsx
@@ -1,14 +1,46 @@
+import { useRef, useState } from "react";
 import "./styles.sass";
 import TwitterIcon from "../TwitterIcon";
+import copyToClipboard from "../../utils/clipboard";
 
 function Box({ quote = "", author = "", generateQuote }) {
+  const textRef = useRef(null);
+  const authorRef = useRef(null);
+  const [copied, setCopied] = useState(false);
+
+  const setTimer = (seconds) => {
+    setTimeout(() => {
+      setCopied(false);
+      console.log("YA!!");
+    }, seconds);
+  };
+
+  const handleCopy = () => {
+    copyToClipboard(`${quote} ${author}`);
+    setCopied(true);
+    setTimer(1000);
+  };
+
+  const handleGenerateQuote = () => {
+    if (textRef.current && authorRef.current) {
+      textRef.current.animate([{ opacity: 0 }, { opacity: 1 }], {
+        duration: 500,
+      });
+      authorRef.current.animate([{ opacity: 0 }, { opacity: 1 }], {
+        duration: 500,
+      });
+    }
+
+    generateQuote();
+  };
+
   return (
     <div id="quote-box" className="Box">
       <div>
-        <p id="text" className="Box-Text">
+        <p id="text" className="Box-Text" ref={textRef}>
           {quote}
         </p>
-        <p id="author" className="Box-Author">
+        <p id="author" className="Box-Author" ref={authorRef}>
           {author}
         </p>
       </div>
@@ -24,9 +56,14 @@ function Box({ quote = "", author = "", generateQuote }) {
             <TwitterIcon />
           </a>
         </div>
-        <button id="new-quote" className="Btn" onClick={generateQuote}>
-          New quote
-        </button>
+        <div className="Box-Buttons">
+          <button className="Btn" onClick={handleCopy}>
+            {copied ? "Copied" : "Copy"}
+          </button>
+          <button id="new-quote" className="Btn" onClick={handleGenerateQuote}>
+            New quote
+          </button>
+        </div>
       </div>
     </div>
   );
